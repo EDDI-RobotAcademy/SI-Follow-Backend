49a631d84429af8456c4c5d92552c911e40d91fc
jung won hyeong <jeongwon264@gmail.com>
2024-10-04 14:02:16 +0900

    feat: [정원형] redis에 사용자의 토큰을 저장합니다. [SIFBD-6]

---
+++ si_follow/redis_service/redis_service.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class RedisService(ABC):
+    @abstractmethod
+    def store_access_token(self, account_id, access_token):
+        pass
+
+    @abstractmethod
+    def get_value_by_key(self, key):
+        pass
+
+    @abstractmethod
+    def delete_key(self, key):
+        pass
+
+    @abstractmethod
+    def generate_and_store_access_token(self,profileRepository, email):
+        pass
+
+    @abstractmethod
+    def delete_access_token(self, userToken):
+        pass
